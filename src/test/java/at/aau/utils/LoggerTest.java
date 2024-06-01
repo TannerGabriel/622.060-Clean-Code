89114a7a4b29c0e3264ed58d5cc484aa1655ed84
Alexander Pichler <alexander.pichler.fi@gmail.com>
2024-06-01 20:21:52 +0200

    fixed some soner issues

--- src/test/java/at/aau/utils/LoggerTest.java
+++ src/test/java/at/aau/utils/LoggerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class LoggerTest {
+class LoggerTest {
 
     @Test
     void testSingletonInstance() {
