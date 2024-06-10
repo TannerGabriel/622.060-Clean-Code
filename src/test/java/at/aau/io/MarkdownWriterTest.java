ca89d841de004a6d314c9cce028cf9c5ef35ccd4
TannerGabriel <gabrieltanner.code@gmail.com>
2024-06-10 10:49:09 +0200

    fix: Sonarcloud issues
    Signed-off-by: TannerGabriel <gabrieltanner.code@gmail.com>

--- src/test/java/at/aau/io/MarkdownWriterTest.java
+++ src/test/java/at/aau/io/MarkdownWriterTest.java
@@ -8,7 +8,6 @@ import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
